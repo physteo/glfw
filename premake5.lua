4c5566a704261e238bbd673fbb59e15779207ed0
physteo <matteo_campo91@live.it>
2020-01-22 16:00:11 +0100

    Updated premake5.lua for linux.

--- premake5.lua
+++ premake5.lua
@@ -19,6 +19,44 @@ project "GLFW"
       "src/window.c"
    }
 
+   filter "system:linux"
+      pic "On"
+   
+      systemversion "latest"
+      staticruntime "On"
+   
+      files
+      {
+   		"src/x11_init.c",
+   		"src/x11_monitor.c",
+   		"src/x11_window.c",
+   		"src/xkb_unicode.c",
+   		"src/posix_time.c",
+   		"src/posix_thread.c",
+   		"src/glx_context.c",
+   		"src/egl_context.c",
+   		"src/osmesa_context.c",
+   		"src/linux_joystick.c"
+      }
+   
+      defines
+      {
+   		"_GLFW_X11"
+      }
+
+   filter "system:linux"
+      systemversion "latest"
+      
+      defines
+      {
+         "_GLFW_X11"
+      }
+ 
+      makesettings [[
+         CXX = g++-7
+      ]]
+
+     
    filter "system:windows"
       systemversion "latest"
 
